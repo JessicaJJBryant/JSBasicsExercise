b7c09e22f500ba1542cb57a76e4e64c81fff6e82
Jessica Bryant <jessica.bryant@live.com>
2021-09-20 18:29:22 -0400

    Complete bonus portion of JSBasicsExercise

--- script.js
+++ script.js
@@ -37,3 +37,33 @@ console.log("JJ".length + "Bryant".length);
 
 // 8
 console.log("Avengers: Endgame"[16]);
+
+// Bonus 1
+console.log("\\");
+
+// Bonus 2
+console.log(5**4-9);
+
+// Bonus 3
+console.log(("I".length - "Am".length + "Iron".length) % "Man".length);
+console.log("I".length -"Am".length + "Iron".length - "Man".length);
+console.log("I".length ** "Am".length - "Iron".length + "Man".length);
+console.log(("I".length + ("Am".length * "Iron".length)) % "Man".length);
+console.log((("I".length + "Am".length) ** "Iron".length) % "Man".length);
+
+// Bonus 4
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY" [0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+// Bonus 5
+let m = 25;
+let c = 4;
+let u = m + c;
+
+
