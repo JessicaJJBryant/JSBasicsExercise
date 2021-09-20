6cdfcb34e9fb333c6215067574f6d53210cf9c4a
Jessica Bryant <jessica.bryant@live.com>
2021-09-20 17:17:16 -0400

    Complete JSBasicsExercise

---
+++ script.js
@@ -0,0 +1,39 @@
+// 1
+console.log(7);
+
+// 2
+/*
+The five main JavaScript data/value types (primitives) are:
+1. number
+2. string
+3. boolean
+4. null
+5. undefined
+*/
+
+// 3
+/*
+The 6 JavaScript operator symbols are:
+1. +
+2. -
+3. *
+4. /
+5. **
+6. %
+*/
+
+//4
+console.log(5%2);
+console.log(5%4);
+
+// 5
+console.log('I\'M MARY POPPINGS, Y\'ALL');
+
+// 6
+console.log("Hawk" + "eye");
+
+// 7
+console.log("JJ".length + "Bryant".length);
+
+// 8
+console.log("Avengers: Endgame"[16]);
