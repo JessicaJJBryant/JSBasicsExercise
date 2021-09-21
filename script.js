b1b9a26437b77e845cccf498bb976815c54b591c
Jessica Bryant <jessica.bryant@live.com>
2021-09-21 10:53:13 -0400

    Add notes & additional variations to exercise

--- script.js
+++ script.js
@@ -50,20 +50,26 @@ console.log("I".length -"Am".length + "Iron".length - "Man".length);
 console.log("I".length ** "Am".length - "Iron".length + "Man".length);
 console.log(("I".length + ("Am".length * "Iron".length)) % "Man".length);
 console.log((("I".length + "Am".length) ** "Iron".length) % "Man".length);
+// I missed this following one.
+console.log("I".length % "Am".length - "Iron".length % "Man".length);
+
 
 // Bonus 4
 console.log("CAP"[1]);
 console.log("VISION"[0]);
 console.log("OKOYE"[4]);
-console.log("NICK FURY" [0]);
+console.log("NICK FURY"[0]);
 console.log("GROOT"[0]);
 console.log("DOCTOR STRANGE"[13]);
 console.log("THOR"[3]);
 console.log("STAR-LORD"[0]);
+// Dave's way
+console.table (["CAP"[1], "VISION"[0], "OKOYE"[4], "NICK FURY"[0], "GROOT"[0], "DOCTOR STRANGE"[13], "THOR"[3], "STAR-LORD"[0]]);
+// Matt's other way all on one console.log line
+console.log ("CAP"[1]+ "\n" + "VISION"[0]+ "\n" + "OKOYE"[4]+ "\n" + "NICK FURY"[0]+ "\n" + "GROOT"[0]+ "\n" + "DOCTOR STRANGE"[13]+ "\n" + "THOR"[3]+ "\n" + "STAR-LORD"[0]);
 
+// Can also use var or const instead of let
 // Bonus 5
 let m = 25;
 let c = 4;
 let u = m + c;
-
-
